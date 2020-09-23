82d8a523977523f783088e93d583b65361cf39e9
Kayla Jean Crane <kayla.crane@gmail.com>
2020-09-23 16:15:24 +0200

    set up store.js

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -4,7 +4,26 @@ import { render } from "react-dom"; /*loads render method that allows us to rend
 //import CSS (webpack handles all loading)
 import css from "./styles/style.styl"; /*all written in stylus*/
 
+//import components
+import Main from "./components/Main";
+import Single from "./components/Single";
+import PhotoGrid from "./components/PhotoGrid";
+
+//import react router dependencies
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
+
+// browserHistory allows us to do push state and change the urls without having to reload page
+//Main is our parent that then gets passed child components through indexRoute at / by default or through a nested route with dynamic id
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+);
+
 render(
-  <p>Howdy</p>,
+  router,
   document.getElementById("root")
-); /*takes two params: jsx and reference to where you want to put that*/
+); /*takes two params: what to render and reference to where you want to put that*/
