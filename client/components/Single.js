d5bde7ab2d1afedc682d976f43360bea528786c8
Kayla Jean Crane <kayla.crane@gmail.com>
2020-10-01 14:28:11 +0200

    add comments section and display comments for each photo

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,8 +1,24 @@
 import React from "react";
+import Photo from "./Photo";
+import Comments from "./Comments";
 
 const Single = React.createClass({
   render() {
-    return <div className="single-photo">single</div>;
+    // get index of clicked post
+    const { postId } = this.props.params;
+    const index = this.props.posts.findIndex((post) => post.code === postId);
+    //get the post
+    const post = this.props.posts[index];
+    const postComments =
+      this.props.comments[postId] ||
+      []; /*in the case that there are no comments we provide an empty []*/
+
+    return (
+      <div className="single-photo">
+        <Photo index={index} post={post} {...this.props} />;
+        <Comments postComments={postComments} />
+      </div>
+    );
   },
 });
 
