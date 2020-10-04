ea0a532e57c0c25efd38e3c1c489f61d4153ed3b
Kayla Jean Crane <kayla.crane@gmail.com>
2020-10-04 19:27:12 +0200

    set up reducer to add and delete comments

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,5 +1,33 @@
+function postComments(state = [], action) {
+  switch (action.type) {
+    case "ADD_COMMENT":
+      return [
+        ...state,
+        {
+          user: action.author,
+          text: action.comment,
+        },
+      ];
+    case "REMOVE_COMMENT":
+      //return new state without deleted comment
+      return [
+        ...state.slice(0, action.index),
+        ...state.slice(action.index + 1),
+      ];
+    default:
+      return state;
+  }
+}
+
 function comments(state = [], action) {
-  // console.log({ state, action });
+  if (typeof action.postId !== "undefined") {
+    return {
+      //take current state and overwrite with new part using another function
+      ...state,
+      [action.postId]: postComments(state[action.postId], action),
+    };
+  }
+
   return state;
 }
 
